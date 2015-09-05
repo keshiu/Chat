fa12421b6a6d3193000e2addf1a8a757b5d9ed0e
Alena Arsenyeva <keshiu@list.ru>
2015-09-05 20:40:49 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+var http = require("http"),
+express = require("express"),
+bodyParser = require("body-parser"),
+app = express(),
+port = proess.env.PORT||3000;
+
+app.use(express.static(__dirname + "/client"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+http.createServer(app).listen(port);
