3eef4cb1135f22ff552ae99c8a3c52229955f69e
Alena Arsenyeva <keshiu@list.ru>
2015-09-14 18:41:12 +0300

    universal port in server file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require("http"),
 express = require("express"),
 bodyParser = require("body-parser"),
 app = express(),
-port = proess.env.PORT||3000;
+port = process.env.PORT||3000;
 
 app.use(express.static(__dirname + "/client"));
 app.use(bodyParser.urlencoded({extended: true}));
